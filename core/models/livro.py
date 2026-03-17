332aa02222ac8aeef50c0632c346a817dbff948b
vitoriadesouza <vitoriasouza.ifc@gmail.com>
2026-03-17 16:11:10 -0300

    FEAT: Inclusão do relacionamento de Livro com Categoria e Editora

--- core/models/livro.py
+++ core/models/livro.py
@@ -1,6 +1,16 @@
 from django.db import models
 
+from .categoria import Categoria
+from .editora import Editora
+
 
 class Livro(models.Model):
-    nome = models.CharField(max_length=100)
-    email = models.EmailField(max_length=100, blank=True, null=True)
+    titulo = models.CharField(max_length=100)
+    isbn = models.CharField(max_length=100, blank=True, null=True)
+    quantidade = models.IntegerField(default=0, null=True, blank=True)
+    preco = models.DecimalField(max_digits=7, decimal_places=2, default=0)
+    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name='livros', null=True, blank=True)
+    editora = models.ForeignKey(Editora, on_delete=models.PROTECT, related_name='livros', null=True, blank=True)
+
+    def __str__(self):
+        return self.titulo
