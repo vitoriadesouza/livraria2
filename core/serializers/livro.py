8da23ced902aa4b074e2d6f82edfd00347c48fc9
vitoriadesouza <vitoriasouza.ifc@gmail.com>
2026-04-07 09:06:46 -0300

    FEAT: Dois serializadores para livro

--- core/serializers/livro.py
+++ core/serializers/livro.py
@@ -3,6 +3,12 @@ from rest_framework.serializers import ModelSerializer
 from core.models import Livro
 
 
+class LivroListRetrieveSerializer(ModelSerializer):
+    class Meta:
+        model = Livro
+        fields = '__all__'
+
+
 class LivroSerializer(ModelSerializer):
     class Meta:
         model = Livro
--- core/views/livro.py
+++ core/views/livro.py
@@ -2,8 +2,14 @@ from rest_framework.viewsets import ModelViewSet
 
 from core.models import Livro
 from core.serializers import LivroSerializer
+from core.serializers.livro import LivroListRetrieveSerializer
 
 
 class LivroViewSet(ModelViewSet):
     queryset = Livro.objects.all()
     serializer_class = LivroSerializer
+
+    def get_serializer_class(self):
+        if self.action in {'list', 'retrieve'}:
+            return LivroListRetrieveSerializer
+        return LivroSerializer
